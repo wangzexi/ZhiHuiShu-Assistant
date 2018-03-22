49c7792d27c2dd452302e78d4ed759584247ad8a
Zexi <zexi@Zexi-MacBook-Pro.local>
2018-03-22 23:10:52 +0800

    :art:格式化代码

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 (function () {
-	const MAX_DELAY = 60;	// 切课时的最大延迟，单位：秒
+	const MAX_DELAY = 60 // 切课时的最大延迟，单位：秒
 
-	const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+	const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
 	const start = async function () {
-		console.log('智慧树助手已启动');
-		while (1) {
-			console.log('正在检查');
-			const delay = Math.floor(Math.random() * MAX_DELAY * 1000) + 1000;
+		console.log('智慧树助手已启动')
+		while (true) {
+			console.log('正在检查')
+			const delay = Math.floor(Math.random() * MAX_DELAY * 1000) + 1000
 
 			if (!/1\.5/.test($('.speedBox').attr('style'))) {
-        console.log('提升到1.5倍速')
-        $('.speedTab15').click()
+				console.log('提升到1.5倍速')
+				$('.speedTab15').click()
 			}
 
 			if ($('.playButton').length > 0) {
-        console.log('点击播放')
-        $('.playButton').click()
-      }
+				console.log('点击播放')
+				$('.playButton').click()
+			}
 
 			if ($('.popbtn_cancel')[0]) {
-				console.log('发现弹题，将在' + delay / 1000 + ' 秒后点击关闭');
-				await sleep(delay);
-				$('.popbtn_cancel').click();
+				console.log('发现弹题，将在' + delay / 1000 + ' 秒后点击关闭')
+				await sleep(delay)
+				$('.popbtn_cancel').click()
 			}
 
 			if ($('.current_play').find('.progressbar').width() == $('.current_play').find('.progressbar_box').width()) {
-				console.log('本节完成，' + delay / 1000 + ' 秒后将切到下一课');
-				await sleep(delay);
-				$('.current_play').nextAll('.video')[0].click();
+				console.log('本节完成，' + delay / 1000 + ' 秒后将切到下一课')
+				await sleep(delay)
+				$('.current_play').nextAll('.video')[0].click()
 			}
 
-			await sleep(1000);
+			await sleep(1000)
 		}
 	}
 
-	start();
-}());
+	start()
+}())
