5d4fff4f24d3ef235ef197d17a5a4196c3b323b9
heiye9 <jiujiuzhang99@qq.com>
2019-03-06 22:49:33 +0800

    :bug:修复弹题点不到选项 (#7)
    弹题使用iframe加载，修改获取元素的方式。

--- main.js
+++ main.js
@@ -34,7 +34,8 @@
 			if ($('.popbtn_cancel')[0]) {
 				console.log('发现弹题，将在' + delay / 1000 + ' 秒后点击关闭')
 				await sleep(delay / 2)
-				$('.answerOption label').click()
+				const iframe = document.getElementById('tmDialog_iframe').contentWindow
+				iframe.document.querySelector('.answerOption label').click()
 				await sleep(delay / 2)
 				$('.popbtn_cancel').click()
 			}
